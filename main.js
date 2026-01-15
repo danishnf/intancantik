115188fb3dd3ae4b6fa191e8176da04d8f2b4c69
danishnf <muhamaddanishnf@gmail.com>
2026-01-15 23:27:57 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-onload = () =>{
+const playButton = document.getElementById('playButton');
+const song = document.getElementById('mySong');
+
+playButton.onclick = () => {
+    // Hilangkan screen tombol
+    document.getElementById('greeting-screen').style.display = 'none';
+    
+    // Munculkan teks ucapan
+    document.getElementById('love-text').style.display = 'block';
+
+    // JALANKAN ANIMASI BUNGA (Menghapus class "container")
     document.body.classList.remove("container");
+
+    // Putar lagu
+    song.play();
 };
